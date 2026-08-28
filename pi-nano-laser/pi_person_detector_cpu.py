37a33ab4e10ddea07f84cb111bf6dbc145394664
Nathaniel Adika <najadika@st.ug.edu.gh>
2026-08-28 10:10:42 +0000

    updated resolutions

--- pi-nano-laser/pi_person_detector_cpu.py
+++ pi-nano-laser/pi_person_detector_cpu.py
@@ -15,8 +15,8 @@ UDP_PORT = 5005
 TARGET_CLASS = "person"
 CONFIDENCE_THRESHOLD = 0.5
 LASER_OFF_DELAY = 3.0         # Seconds to keep laser on after last detection
-FRAME_WIDTH = 640
-FRAME_HEIGHT = 480
+FRAME_WIDTH = 1280
+FRAME_HEIGHT = 720
 VIDEO_FPS = 60
 
 # Session folder
