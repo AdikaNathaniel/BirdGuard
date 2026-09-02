9f6b2ca2d3f23031b1d3a45ebe54ce0f4885a3b5
Nathaniel Adika <najadika@st.ug.edu.gh>
2026-09-02 09:59:24 +0000

    pi updates

--- PI-SERVO-FORMULA-TRIALS/pi_servo_formula_trial_2.py
+++ PI-SERVO-FORMULA-TRIALS/pi_servo_formula_trial_2.py
@@ -1,9 +1,9 @@
 from gpiozero import AngularServo
 from time import sleep
 
-# Initialize the servo on GPIO pin 14
+# Initialize the servo on GPIO pin 15
 # min_pulse_width and max_pulse_width may need to be adjusted for your servo
-servo = AngularServo(14, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+servo = AngularServo(15, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
 
 
 # Function to set the servo angle
